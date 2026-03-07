4d6ca3b70cdbc4b4fe4f5dc380691ca338a0311f
Nyuminh <minhdlhse180733@fpt.edu.vn>
2026-03-07 18:39:55 +0700

    some thing

---
+++ Assets/Core/Scripts/Runtime/GameEvents/LionDamageDealer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Blocks.Gameplay.Core; // Để sử dụng HitInfo và IHittable
+
+public class LionDamageDealer : MonoBehaviour
+{
+    public float damageAmount = 10f;
+    public float pushForceMagnitude = 5f;
+
+    public void DealDamage(GameObject target)
+    {
+
+        var hittable = target.GetComponent<IHittable>();
+
+        if (hittable != null)
+        {
+            Debug.Log("Sư tử đang vồ!");
+            // Cấu trúc chuẩn theo file bạn gửi
+            HitInfo info = new HitInfo
+            {
+                amount = damageAmount, // Trong template của bạn dùng 'amount' thay vì 'damage'
+                hitPoint = target.transform.position,
+                hitNormal = (target.transform.position - transform.position).normalized,
+                attackerId = 0, // ID của sư tử (0 là mặc định)
+                impactForce = (target.transform.position - transform.position).normalized * pushForceMagnitude // Dùng Vector3
+            };
+
+            hittable.OnHit(info);
+        }
+    }
+}
---
+++ Assets/Core/Scripts/Runtime/GameEvents/Menu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    [Header("Settings")]
+    public string campaignSceneName = "CampaignScene";
+    public string multiplayerSceneName = "MultiplayerScene";
+
+    public void PlayGame()
+    {
+        Debug.Log("Bắt đầu Game...");
+        // Chuyển đến màn hình chọn nhân vật hoặc vào game trực tiếp
+    }
+
+    public void OpenOptions()
+    {
+        Debug.Log("Mở cài đặt");
+    }
+
+    public void OpenCredits()
+    {
+        Debug.Log("Mở thông tin tác giả");
+    }
+
+    public void SelectCharacter()
+    {
+        Debug.Log("Mở menu chọn nhân vật");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Thoát game");
+        Application.Quit();
+    }
+}
